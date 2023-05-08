4bd17889a24745371a0c8fb4a24a5e903a10cbfa
Rehman Javaid <rehmanjav@outlook.com>
2023-05-08 18:10:07 -0400

    WIP: Image slider

--- image-slider/index.js
+++ image-slider/index.js
@@ -1,1 +1,23 @@
 "use strict";
+
+let divRibbon = document.querySelector(".ribbon");
+let btnLeft = document.querySelector(".btn-left");
+let btnRight = document.querySelector(".btn-right");
+let imgs = document.querySelectorAll(".ribbon > img");
+let numImgs = imgs.length;
+let heightImgs = imgs[0].height
+let ribbonEnd = (numImgs - 1) * heightImgs * -1;
+
+btnLeft.addEventListener('click', () => {
+    let ribbonTop = parseInt(getComputedStyle(divRibbon).top);
+    if (ribbonTop < 0) {
+        divRibbon.style.top = `${ribbonTop + heightImgs}px`;
+    }
+});
+btnRight.addEventListener('click', () => {
+    let ribbonTop = parseInt(getComputedStyle(divRibbon).top);
+    if (ribbonTop > ribbonEnd) {
+        divRibbon.style.top = `${ribbonTop - heightImgs}px`;
+    }
+});
+
