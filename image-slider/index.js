a7452b7e6f2fbdaeac9bd48926679f290ce3dc27
Rehman Javaid <rehmanjav@outlook.com>
2023-10-01 17:03:32 -0400

    Complete image-slider button logic

--- image-slider/index.js
+++ image-slider/index.js
@@ -6,18 +6,23 @@ let btnRight = document.querySelector(".btn-right");
 let imgs = document.querySelectorAll(".ribbon > img");
 let numImgs = imgs.length;
 let heightImgs = imgs[0].height
-let ribbonEnd = (numImgs - 1) * heightImgs * -1;
+let ribbonEnd = (numImgs - 1) * heightImgs * -1;            // top of last image
+let totalLength = numImgs * heightImgs;
 
 btnLeft.addEventListener('click', () => {
     let ribbonTop = parseInt(getComputedStyle(divRibbon).top);
     if (ribbonTop < 0) {
         divRibbon.style.top = `${ribbonTop + heightImgs}px`;
+    } else if (ribbonTop == 0) {
+        divRibbon.style.top = `${ribbonEnd}px`;
     }
 });
 btnRight.addEventListener('click', () => {
     let ribbonTop = parseInt(getComputedStyle(divRibbon).top);
     if (ribbonTop > ribbonEnd) {
         divRibbon.style.top = `${ribbonTop - heightImgs}px`;
+    } else if (ribbonTop == ribbonEnd) {
+        divRibbon.style.top = `0px`;
     }
 });
 
